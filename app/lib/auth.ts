eab16ca7b83839d856e07bd20d275bb70bbfbb9f
lzh2000206 <lzh2000206@gmail.com>
2026-05-08 21:16:21 +0800

    request github email scope

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -104,6 +104,11 @@ export const {
       clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET,
       allowDangerousEmailAccountLinking: true,
+      authorization: {
+        params: {
+          scope: 'read:user user:email',
+        },
+      },
     }),
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
